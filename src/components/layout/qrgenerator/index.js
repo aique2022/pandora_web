17de2620ac625302741df598a43c9c41d4c20725
aique2022 <115211398+aique2022@users.noreply.github.com>
2023-05-03 11:51:05 +0800

    reupload

--- src/components/layout/qrgenerator/index.js
+++ src/components/layout/qrgenerator/index.js
@@ -1,15 +1,25 @@
 
 
 import { useEffect, useState } from "react";
-import QRCode from "react-qr-code";
+import { useQRCode } from 'next-qrcode';
 const QRCodeGenerator = ({quickpin}) => {
     const [qr, setQr] = useState('')
+    const { SVG } = useQRCode();
     useEffect(() => {
         if(quickpin != "") {
-            setQr(<QRCode value={quickpin}
-                size={20}
-                style={{ height: "auto", maxWidth: "100%", width: "30%", margin: "0 auto" }}
-    viewBox={`0 0 100% 100%`}/>)
+            setQr(
+                <SVG
+                text={quickpin}
+                options={{
+                  margin: 2,
+                  width: 300,
+                  color: {
+                    dark: '#000',
+                    light: '#fff',
+                  },
+                }}
+                
+              />)
         }
     },[])
     return (
